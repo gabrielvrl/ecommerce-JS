6dc395215b8306538cb9c8f237cb473de584e3b4
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-13 07:46:30 -0300

    Creating a web Server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('bye there!')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
