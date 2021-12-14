b4a257e1e3a2cf55bb9756c0e109ce29fe7e117f
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-14 08:28:19 -0300

    Finding By Id

--- repositories/users.js
+++ repositories/users.js
@@ -37,19 +37,19 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+
+  async getOne(id){
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.create({
-    email: 'test@test.com',
-    password: 'password'
-  });
+  const user = await repo.getOne('f3546695');
 
-  const users = await repo.getAll();
-
-  console.log(users)
+  console.log(user)
 };
 
 test();
