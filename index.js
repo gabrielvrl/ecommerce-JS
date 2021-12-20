034089ada59445288ce66722fedc8a87961ef1f2
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-12-20 07:56:59 -0300

    Signout a user

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cookieSession({
   keys: ['jdjnsdnlsdaln']
 }));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
       Your id is: ${req.session.userId}
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
   const { email, password, passwordConfirmation } = req.body
 
   const existingUser = await usersRepo.getOneBy({ email });
@@ -43,6 +43,29 @@ app.post('/', async (req, res) => {
 
   res.send('Account created!')
 })
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+
+  res.send('You are logged out')
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      Your id is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `)
+});
+
+app.post('/signin', async (req, res) => {
+  
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
